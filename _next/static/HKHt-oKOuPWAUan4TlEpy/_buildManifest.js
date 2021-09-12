029fee0ba454ca42bb5d78b3c237e8dfae2af07e
pserb <61565464+pserb@users.noreply.github.com>
2021-09-12 05:06:40 +0000

    Deploying to gh-pages from @ pserb/stuyjucoc-frontend@b2c4933a878289f3a52b33d00eecfb3055af5c74 🚀

---
+++ _next/static/HKHt-oKOuPWAUan4TlEpy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2c345b4322ff2108d12c.css","static/chunks/pages/index-29de4be1cbe2dbf75505.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
